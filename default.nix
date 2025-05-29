37b5d088a78240faeb187eac45bcd4286bc00857
Regan Anderson <llisandur@outlook.com>
2025-05-29 11:06:44 -0500

    initial

---
+++ default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchurl,
+}:
+
+stdenvNoCC.mkDerivation rec {
+  pname = "eorzean-typeface";
+  version = "1.051";
+
+  src = fetchurl {
+    url = "https://github.com/karaipsum/Eorzean-Typeface/releases/download/v${version}/AugmentedNeo-Eorzean-Regular.otf";
+    hash = "sha256-SeIJ6uQjCeV0IwJoSt557nIHBk1l6J+N7ef7LzS2ugs=";
+  };
+
+  dontUnpack = true;
+
+  installPhase = ''
+    runHook preInstall
+    install -Dm644 $src -t $out/share/fonts/opentype
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Augmented Neo-Eorzean";
+    homepage = "https://github.com/karaipsum/Eorzean-Typeface";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.all;
+  };
+}
